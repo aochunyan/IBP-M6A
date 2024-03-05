8ded62a2917eaaba0e6a27611353cc2f27f51d04
aochunyan <73344781+aochunyan@users.noreply.github.com>
2024-03-05 22:11:52 +0800

    Add files via upload

---
+++ model/Bert_model.py
@@ -0,0 +1,153 @@
+# ---encoding:utf-8---
+# @Time : 2020.11.09
+# @Author : Waiting涙
+# @Email : 1773432827@qq.com
+# @IDE : PyCharm
+# @File : ACPred-LAF-Basic.py
+
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+max_len =103
+n_layers = 2
+n_head = 8
+d_model = 32
+d_ff = 128
+d_k = 16
+d_v = 16
+vocab_size = 136
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def get_attn_pad_mask(seq):
+    batch_size, seq_len = seq.size()
+    pad_attn_mask = seq.data.eq(0).unsqueeze(1)  # [batch_size, 1, seq_len]
+    pad_attn_mask_expand = pad_attn_mask.expand(batch_size, seq_len, seq_len)  # [batch_size, seq_len, seq_len]
+    return pad_attn_mask_expand
+
+
+class Embedding(nn.Module):
+    def __init__(self):
+        super(Embedding, self).__init__()
+        self.tok_embed = nn.Embedding(vocab_size, d_model)  # token embedding (look-up table)
+        self.pos_embed = nn.Embedding(max_len, d_model)  # position embedding
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(self, x):
+        seq_len = x.size(1)  # x: [batch_size, seq_len]
+        pos = torch.arange(seq_len, device=device, dtype=torch.long)  # [seq_len]
+        pos = pos.unsqueeze(0).expand_as(x)  # [seq_len] -> [batch_size, seq_len]
+        embedding = self.pos_embed(pos)
+        embedding = embedding + self.tok_embed(x)
+        embedding = self.norm(embedding)
+        return embedding
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super(ScaledDotProductAttention, self).__init__()
+
+    def forward(self, Q, K, V, attn_mask):
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k)  # scores : [batch_size, n_head, seq_len, seq_len]
+        scores.masked_fill_(attn_mask, -1e9)  # Fills elements of self tensor with value where mask is one.
+        attn = nn.Softmax(dim=-1)(scores)  # [batch_size, n_head, seq_len, seq_len]
+        context = torch.matmul(attn, V)  # [batch_size, n_head, seq_len, d_v]
+        return context, attn
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super(MultiHeadAttention, self).__init__()
+        self.W_Q = nn.Linear(d_model, d_k * n_head)
+        self.W_K = nn.Linear(d_model, d_k * n_head)
+        self.W_V = nn.Linear(d_model, d_v * n_head)
+
+        self.linear = nn.Linear(n_head * d_v, d_model)
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(self, Q, K, V, attn_mask):
+        residual, batch_size = Q, Q.size(0)
+        q_s = self.W_Q(Q).view(batch_size, -1, n_head, d_k).transpose(1, 2)  # q_s: [batch_size, n_head, seq_len, d_k]
+        k_s = self.W_K(K).view(batch_size, -1, n_head, d_k).transpose(1, 2)  # k_s: [batch_size, n_head, seq_len, d_k]
+        v_s = self.W_V(V).view(batch_size, -1, n_head, d_v).transpose(1, 2)  # v_s: [batch_size, n_head, seq_len, d_v]
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_head, 1, 1)
+        context, attention_map = ScaledDotProductAttention()(q_s, k_s, v_s, attn_mask)
+        context = context.transpose(1, 2).contiguous().view(batch_size, -1,
+                                                            n_head * d_v)  # context: [batch_size, seq_len, n_head * d_v]
+        output = self.linear(context)
+        output = self.norm(output + residual)
+        return output, attention_map
+
+
+class PoswiseFeedForwardNet(nn.Module):
+    def __init__(self):
+        super(PoswiseFeedForwardNet, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # (batch_size, seq_len, d_model) -> (batch_size, seq_len, d_ff) -> (batch_size, seq_len, d_model)
+        return self.fc2(self.relu(self.fc1(x)))
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        super(EncoderLayer, self).__init__()
+        self.enc_self_attn = MultiHeadAttention()
+        self.pos_ffn = PoswiseFeedForwardNet()
+        self.attention_map = None
+
+    def forward(self, enc_inputs, enc_self_attn_mask):
+        enc_outputs, attention_map = self.enc_self_attn(enc_inputs, enc_inputs, enc_inputs,
+                                                        enc_self_attn_mask)  # enc_inputs to same Q,K,V
+        self.attention_map = attention_map
+        enc_outputs = self.pos_ffn(enc_outputs)  # enc_outputs: [batch_size, seq_len, d_model]
+        return enc_outputs
+
+
+class BERT(nn.Module):
+    def __init__(self):
+        super(BERT, self).__init__()
+
+
+        self.embedding1 = Embedding()
+
+        self.layers1 = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])
+
+
+        self.block1 = nn.Sequential(
+            nn.Linear(d_model, d_model//2),
+            nn.BatchNorm1d(d_model//2),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(d_model//2, 2),
+        )
+        
+
+
+
+
+    def forward(self, input_ids):
+        batch_size, seq_len = input_ids.size()
+        output = self.embedding1(input_ids)  # [bach_size, seq_len, d_model]
+
+        enc_self_attn_mask = get_attn_pad_mask(input_ids)  # [batch_size, maxlen, maxlen]
+
+        for layer1 in self.layers1:
+            output = layer1(output, enc_self_attn_mask)
+
+
+        # output: [batch_size, max_len, d_model]
+        # classification
+        # only use [CLS]
+        output = output[:, 0, :]
+        outputs = self.block1(output)
+
+        return outputs
+    
+
+    
+    
